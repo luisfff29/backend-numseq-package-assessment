9241a91e343870abbc14b3e6003cec2c0a39be09
luisfff29 <luisfff29@gmail.com>
2020-03-17 08:53:04 -0500

    def triangle done

--- numseq/geo.py
+++ numseq/geo.py
@@ -3,7 +3,15 @@ def square(n):
 
 
 def triangle(n):
-    pass
+    if n != 0 and n != 1:
+        constant = 2
+        result = 1
+        for _ in range(n-1):
+            result = result + constant
+            constant += 1
+        return result
+    else:
+        return n
 
 
 def cube(n):
