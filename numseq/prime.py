976f59e5040de23559ba51dbf351882b468939f6
luisfff29 <luisfff29@gmail.com>
2020-03-18 00:12:45 -0500

    def is_prime done

--- numseq/prime.py
+++ numseq/prime.py
@@ -1,3 +1,21 @@
+import math
+
+
+def is_prime(m):
+    """Returns a boolean indicating whether m is a prime number"""
+    if m < 2:
+        return False
+    elif m == 2:
+        return True
+    elif m % 2 == 0:
+        return False
+    else:
+        for i in range(3, 1 + int(math.sqrt(m)), 2):
+            if m % i == 0:
+                return False
+        return True
+
+
 def primes(n):
     """Returns a list of all prime numbers less than n"""
     my_list = []
